66c37dae313e83481caf8fad33e45b78ef9971a5
metndurn <mtndrn123@gmail.com>
2025-03-06 21:38:45 +0300

    Add project files.

---
+++ BlogProject/Context/BlogDbContext.cs
@@ -0,0 +1,17 @@
+﻿using BlogProject.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlogProject.Context
+{
+	/*veritabanı baglantısını olusturmak ıcın bu yapıyı kullanıyoruz bu yapının adı codefirst*/
+	public class BlogDbContext : DbContext
+	{
+		/*veritabanını comfigöre edecegız*/
+		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+		{
+			//database yolunu ve adını belirtiyoruz
+			optionsBuilder.UseSqlServer("Data Source = .; Database = BlogDb; Integrated Security = true; TrustServerCertificate = True;");
+		}
+		public DbSet<Blog> Blogs { get; set; }//veritabanında ki adını burada belirtip hangi tabloya baglanacagını belirtiyoruz
+	}
+}
---
+++ BlogProject/Migrations/20250306180301_m1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BlogProject.Migrations
+{
+    /// <inheritdoc />
+    public partial class m1 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {/*tablo olusturuldu yani olusturduk*/
+            migrationBuilder.CreateTable(
+                name: "Blogs",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ImageUrl = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    PublishDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Tags = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    LikeCount = table.Column<int>(type: "int", nullable: false),
+                    CommentCount = table.Column<int>(type: "int", nullable: false),
+                    ViewCount = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Blogs", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {/*tabloyu yazdırdık*/
+            migrationBuilder.DropTable(
+                name: "Blogs");
+        }
+    }
+}
