56a3bf45448ff89c7e70ba2bf5f3c12a741a8e4b
metndurn <mtndrn123@gmail.com>
2025-03-07 18:43:46 +0300

    ..

---
+++ BlogProject/Controllers/BlogsController.cs
@@ -0,0 +1,29 @@
+﻿using BlogProject.Context;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlogProject.Controllers
+{
+	public class BlogsController : Controller
+	{
+		//veri tabanı baglantısı olusturuldu
+		private readonly BlogDbContext _context;
+
+		public BlogsController(BlogDbContext context)//alınan verıtabanı baglantısı ıcın constructor olusturuldu
+		{
+			_context = context;
+		}
+
+		public IActionResult Index()//blog anasayfası ıcın liste olusturuldu
+		{
+			//var blogs = _context.Blogs.ToList(); //liste halinde veritabanındaki verileri alıyoruz
+			var blogs = _context.Blogs.Where(x => x.Status == 1).ToList(); //sadece aktif olanları listelemek ıcın where sorgusu yazıldı
+			return View(blogs);
+		}
+
+		public IActionResult Details(int id)//blog detay sayfası ıcın olusturuldu parametre verdık cunku her blogun ıd sı farklı olacak
+		{
+			var blog = _context.Blogs.Where(x => x.Id == id).FirstOrDefault();//veritabanından ıd ye gore blogu cekıyoruz .FirstOrDefault() bir tane demektir
+			return View(blog);
+		}
+	}
+}
--- BlogProject/Program.cs
+++ BlogProject/Program.cs
@@ -27,6 +27,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Blogs}/{action=Index}/{id?}");
 
 app.Run();
