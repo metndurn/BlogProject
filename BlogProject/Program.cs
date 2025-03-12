26fb5af29eb8713d6dbd9cb313663731d2e7a613
metndurn <mtndrn123@gmail.com>
2025-03-12 06:31:32 +0300

    admin temasi duzeltildi

--- BlogProject/Program.cs
+++ BlogProject/Program.cs
@@ -27,6 +27,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Blogs}/{action=Index}/{id?}");
+    pattern: "{controller=Admin}/{action=Index}/{id?}");
 
 app.Run();
